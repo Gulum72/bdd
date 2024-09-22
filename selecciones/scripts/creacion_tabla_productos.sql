a740dfef399a5e25e0b52995d47d986d75342472
Gulum72 <freiresteven72@gmail.com>
2024-09-22 18:46:19 -0500

    agregando scripts a la carpeta selecciones

---
+++ selecciones/scripts/creacion_tabla_productos.sql
@@ -0,0 +1,41 @@
+drop table clientes
+--creando tablas
+
+drop table productos
+--primera tabla
+create table productos(
+codigo int not null,
+nombre varchar(50) not null,
+descripcion varchar(200),
+precio money not null,
+stock int not null,
+constraint productos_pk primary key(codigo)
+)
+--1
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values (1,'royal','polvo royal para hornear',2.50,6)
+--2
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values (2,'crema nivea','crema para peinar cabello',4.60,8)
+--3
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values (3,'doritos','dulce artificial',1,10)
+--4
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values (4,'coca cola','bebida refrescante',3.00,11)
+--5
+insert into productos(codigo,nombre,descripcion,precio,stock)
+values (5,'pilsener','bebida alcoholica',2.50,20)
+--6
+insert into productos(codigo,nombre,precio,stock)
+values (6,'pony malta',1.50,10)
+--7
+insert into productos(codigo,nombre,precio,stock)
+values (7,'manicris',1.20,22)
+--8
+insert into productos(codigo,nombre,precio,stock)
+values (8,'chupete',0.25,13)
+
+
+--mostrar columnas de la tabla
+select * from productos
